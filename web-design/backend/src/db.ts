c521e6d709ff90235e352a0590ab67e9d06c9604
Lajiao <angel141001@hotmail.com>
2025-01-27 21:57:49 +0800

    update

--- web-design/backend/src/db.ts
+++ web-design/backend/src/db.ts
@@ -127,7 +127,9 @@ export const initializeSessionsData = async () => {
     } catch (error) {
         console.error('Sessions 初始化失敗:', error);
     }
+    initializeSessionsData()
 };
+
 
 
 async function initialize() {
